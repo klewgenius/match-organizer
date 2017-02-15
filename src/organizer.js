cc01d5a90834cbbfffd3ab7996b62b6ad412612a
Andrés Baez <abaez@carrasco.uruit.com>
2017-02-15 15:29:20 -0300

    refactor and unnecessary stuff removed

--- src/organizer.js
+++ src/organizer.js
@@ -2,21 +2,6 @@
  * Created by Andrés Báez on 14/2/2017.
  */
 
-var players = [
-    { name: "Andres", score: 4},
-    { name: "Waldemar", score: 2},
-    { name: "Gonzalo", score: 5},
-    { name: "Paco", score: 2},
-    { name: "Charly", score: 5},
-    { name: "Pelu", score: 4},
-    { name: "Joaco", score: 3},
-    { name: "Guti", score: 1},
-    { name: "Carlico", score: 1},
-    { name: "Rivera", score: 4},
-
-];
-
-
 var Organizer = (function(){
 
     var organizer = {};
@@ -27,110 +12,25 @@ var Organizer = (function(){
     }
 
     function print(){
-        var result = getEvenGroups();
+
+        var result = createTeams(organizer.players);
 
         console.log("DIFF= " + result.diff);
         console.log(" ");
         console.log("Team A (" + result.scoreA + ")");
         console.log("---------------------")
-        for(i = 0; i<result.teamA.length; i++){
+        for(i = 0; i < result.teamA.length; i++){
             console.log(result.teamA[i].name + " - " + result.teamA[i].score);
         }
         console.log("");
         console.log("Team B (" + result.scoreB + ")");
         console.log("---------------------")
-        for(i = 0; i<result.teamB.length; i++){
+        for(i = 0; i < result.teamB.length; i++){
             console.log(result.teamB[i].name + " - " + result.teamB[i].score);
         }
     }
 
-    function sum(total, b) {
-        return total + b.score;
-    }
-
-    function getEvenGroups() {
-
-        //var list = [];
-
-        var teams = loftPermute(organizer.players);
-
-        var scoreTeamA = teams[0].reduce(sum, 0);
-        var scoreTeamB = teams[1].reduce(sum, 0);
-
-        var diff = Math.abs(scoreTeamA - scoreTeamB)
-
-        return {
-            teamA: teams[0],
-            teamB: teams[1],
-            diff: diff,
-            scoreA: scoreTeamA,
-            scoreB: scoreTeamB
-        };
-
-        /*var teamA = teams[0],
-         teamB = teams[1];
-
-         var permutations = permute(organizer.players);
-         var permutations = loftPermute;
-
-         for(i = 0; i< permutations.length; i++){
-
-         var teams = createTeams(permutations[i]);
-         var teamA = teams[0],
-         teamB = teams[1];
-
-         var scoreTeamA = teamA.reduce(sum, 0);
-         var scoreTeamB = teamB.reduce(sum, 0);
-
-         var diff = Math.abs(scoreTeamA - scoreTeamB)
-
-         if(!teamIsAlreadyInArray(list,teamA)) {
-         list.push({
-         teamA: teamA,
-         teamB: teamB,
-         diff: diff
-         });
-         }
-         }
-         return list.sort((a, b) => a.diff - b.diff);
-         }
-
-         function teamIsAlreadyInArray(list, team) {
-
-         return false;
-         /*
-         var found = false;
-         for(var i = 0; i < vendors.length; i++) {
-         if (vendors[i].Name == 'Magenic') {
-         found = true;
-         break;
-         }
-         }
-
-         }
-
-         //Fix this. It takes too long
-         var permArr = [],
-         usedItems = [];
-
-         function permute(input) {
-         var i, itemSelected;
-
-         for (i = 0; i < input.length; i++) {
-         itemSelected = input.splice(i, 1)[0];
-         usedItems.push(itemSelected);
-         if (input.length == 0) {
-         permArr.push(usedItems.slice());
-         }
-         permute(input);
-         input.splice(i, 0, itemSelected);
-         usedItems.pop();
-         }
-         return permArr
-         }
-         */
-    }
-    function loftPermute(input) {
+    function createTeams(input) {
 
         var teama = [],
             teamb = [],
@@ -139,7 +39,7 @@ var Organizer = (function(){
             scoreTeamB = 0,
             max = input.length;
 
-        input.sort((a, b) => b.score - a.score); //order desc by score
+        input.sort((a, b) => b.score - a.score);
 
         for (i = 0; i < max; i++){
 
@@ -154,27 +54,39 @@ var Organizer = (function(){
             }
         }
 
-        return [teama, teamb];
+        return {
+            teamA: teama,
+            teamB: teamb,
+            diff: Math.abs(scoreTeamA - scoreTeamB),
+            scoreA: scoreTeamA,
+            scoreB: scoreTeamB
+        };
+
     }
-
-    function createTeams(allPlayers) {
-        var i = 0,
-            l = allPlayers.length,
-            n = l / 2;
-
-        return [allPlayers.slice(0, n),
-            allPlayers.slice(n, l)]
-    }
-
 
     return {
         setPlayers: setPlayers,
-        run: getEvenGroups,
+        run: createTeams,
         print: print
     }
 
 })();
 
+var players = [
+    { name: "Andres", score: 3.8},
+    { name: "Waldemar", score: 2},
+    { name: "Gonzalo", score: 5},
+    { name: "Paco", score: 2},
+    { name: "Charly", score: 5},
+    { name: "Pelu", score: 4},
+    { name: "Joaco", score: 3},
+    { name: "Guti", score: 1},
+    { name: "Carlico", score: 1},
+    { name: "Rivera", score: 4},
+    { name: "Milton", score: 3},
+    { name: "Colo", score: 2}
+
+];
 
 Organizer.setPlayers(players);
 Organizer.print();
