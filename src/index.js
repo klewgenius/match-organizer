2fd04a524e29ab67e8b5446b9e66cae86b5bb92b
Andrés Baez <abaez@carrasco.uruit.com>
2017-02-19 01:18:40 -0300

    writing node package

---
+++ src/index.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Andrés Báez on 14/2/2017.
+ */
+
+//var players = require("./players.json");
+var players = [];
+
+module.exports = {
+    setPlayers : setPlayers,
+    createTeams: createTeams
+};
+
+function setPlayers(players){
+
+    players = players;
+
+}
+
+function createTeams() {
+
+    var input = players;
+
+    var teams = [],
+        i = 0,
+        scores = [],
+        max = input.length;
+
+    scores["A"]=0; scores["B"]=0;
+    teams["A"]=[]; teams["B"]=[];
+
+    input.sort((a, b) => b.score - a.score);
+
+    var nextPlayer;
+    var teamForNextPlayer;
+
+    for (i = 0; i < max; i++){
+
+        next();
+        teams[teamForNextPlayer].push(nextPlayer);
+        scores[teamForNextPlayer] += nextPlayer.score;
+    }
+
+    function getBest() {
+        nextPlayer = input.splice(0, 1)[0]; //Best in list
+    }
+    function getWorst() {
+        nextPlayer =  input.splice(input.length - 1, 1)[0]; //Worst in list
+    }
+    function next(){
+        var counterA = teams["A"],
+            counterB = teams["B"],
+            scoreA = scores["A"],
+            scoreB = scores["B"];
+
+        if(counterA > counterB) { //B has less players than A
+            teamForNextPlayer = "B";
+
+            if(scoreB >= scoreA){
+                getWorst();
+            }else{
+                getBest();
+            }
+        }
+        else if(counterA < counterB) { //A has less players than B
+            teamForNextPlayer = "A";
+            if(scoreA >= scoreB){
+                getWorst();
+            }else{
+                getBest();
+            }
+        }
+        else //Same amount of players
+        {
+            getBest();
+            teamForNextPlayer = (scoreA >= scoreB) ? "B" : "A";
+        }
+    }
+
+    return {
+        teamA: teams["A"],
+        teamB: teams["B"],
+        diff: Math.abs(scores["A"] - scores["B"]),
+        scoreA: scores["A"],
+        scoreB: scores["B"]
+    };
+
+}
